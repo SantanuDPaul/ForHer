a143aad6d4dd01a4b40da36af28d72eb62995fcf
Santanu Paul <santanupaul366@gmail.com>
2026-07-12 11:17:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,129 @@
+let highestZ = 1;
+
+class Paper {
+
+  constructor() {
+
+    this.holdingPaper = false;
+
+    this.pointerStartX = 0;
+    this.pointerStartY = 0;
+
+    this.pointerX = 0;
+    this.pointerY = 0;
+
+    this.prevPointerX = 0;
+    this.prevPointerY = 0;
+
+    this.velX = 0;
+    this.velY = 0;
+
+    this.currentPaperX = Math.random() * 50 - 25;
+    this.currentPaperY = Math.random() * 50 - 25;
+
+    this.rotation = Math.random() * 30 - 15;
+
+  }
+
+  init(paper) {
+    paper.style.transform =
+      `
+      translateX(${this.currentPaperX}px)
+      translateY(${this.currentPaperY}px)
+      rotateZ(${this.rotation}deg)
+      `;
+    paper.addEventListener('pointerdown', (e) => {
+
+      if (paper.classList.contains('heart')) return;
+
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ++;
+
+      this.pointerStartX = e.clientX;
+      this.pointerStartY = e.clientY;
+
+      this.prevPointerX = e.clientX;
+      this.prevPointerY = e.clientY;
+    });
+
+    document.addEventListener('pointermove', (e) => {
+
+      if (!this.holdingPaper) return;
+
+      this.pointerX = e.clientX;
+      this.pointerY = e.clientY;
+
+      this.velX = this.pointerX - this.prevPointerX;
+      this.velY = this.pointerY - this.prevPointerY;
+
+      this.currentPaperX += this.velX;
+      this.currentPaperY += this.velY;
+
+      this.prevPointerX = this.pointerX;
+      this.prevPointerY = this.pointerY;
+
+      paper.style.transform =
+        `translateX(${this.currentPaperX}px)
+     translateY(${this.currentPaperY}px)
+     rotateZ(${this.rotation}deg)`;
+    });
+
+    document.addEventListener('pointerup', () => {
+
+      this.holdingPaper = false;
+
+    });
+
+
+  }
+
+}
+
+const papers = Array.from(document.querySelectorAll('.paper'));
+
+papers.forEach(paper => {
+
+  if (paper.classList.contains('heart')) return;
+
+  const p = new Paper();
+
+  p.init(paper);
+
+});
+
+const heart = document.getElementById("heartLetter");
+const modal = document.getElementById("letterModal");
+const closeBtn = document.getElementById("closeLetter");
+
+if (heart && modal) {
+
+  heart.addEventListener("click", () => {
+
+
+    modal.style.display = "flex";
+
+
+  });
+
+}
+
+if (closeBtn) {
+
+  closeBtn.addEventListener("click", () => {
+
+
+    modal.style.display = "none";
+
+
+  });
+  modal.addEventListener("click", (e) => {
+
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+
+});
+
+}
